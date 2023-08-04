56355be69179bddbbd69e7d1e860b0bab90ba452
LetMeFly666 <814114971@qq.com>
2023-08-04 21:24:57 +0800

    关闭了窗口显示，但是，，，有默认UI诶，很诡异反正是

--- Codes/RestReminder.cpp
+++ Codes/RestReminder.cpp
@@ -5,6 +5,8 @@
 RestReminder::RestReminder(QApplication* a, QWidget *parent) : QMainWindow(parent) {
     ui.setupUi(this);
     controller.setQApplication(a);
+    setAttribute(Qt::WA_TranslucentBackground);
+    setWindowFlags(Qt::FramelessWindowHint);  // 此行windows下需
 }
 
 RestReminder::~RestReminder() {}
