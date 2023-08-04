e3d5d2c5e93de5f643ebf8121db8c8e5afff8153
LetMeFly666 <814114971@qq.com>
2023-08-04 21:14:54 +0800

    替换所有tab为4space

--- Codes/Controller.h
+++ Codes/Controller.h
@@ -9,106 +9,106 @@
 #include <QMenu>
 
 enum Status {
-	worktime = 1,
-	worktime_timeout = 2,
-	resttime = 3,
-	rest_timeout = 4
+    worktime = 1,
+    worktime_timeout = 2,
+    resttime = 3,
+    rest_timeout = 4
 };
 
 class Controller {
 private:
-	QApplication* a = nullptr;
-	Status status = worktime;
-	int secCnt = 0;
-	Config config;
-	QSystemTrayIcon* trayIcon;
-	QMenu* menu;
-	QAction* workAction;
-	QAction* restAction;
-	QAction* exitAction;
+    QApplication* a = nullptr;
+    Status status = worktime;
+    int secCnt = 0;
+    Config config;
+    QSystemTrayIcon* trayIcon;
+    QMenu* menu;
+    QAction* workAction;
+    QAction* restAction;
+    QAction* exitAction;
 
-	int getStatus() {
-		return status;
-	}
+    int getStatus() {
+        return status;
+    }
 
-	int getCntTime() {
-		return secCnt;
-	}
+    int getCntTime() {
+        return secCnt;
+    }
 
-	void setStatus(Status status) {
-		this->status = status;
-		secCnt = 0;
-	}
+    void setStatus(Status status) {
+        this->status = status;
+        secCnt = 0;
+    }
 
-	void startWork() {
-		setStatus(worktime);
-		menu->removeAction(restAction);
-		menu->removeAction(exitAction);
-		menu->addAction(workAction);
-		menu->addAction(exitAction);
-	}
+    void startWork() {
+        setStatus(worktime);
+        menu->removeAction(restAction);
+        menu->removeAction(exitAction);
+        menu->addAction(workAction);
+        menu->addAction(exitAction);
+    }
 
-	void stopWork() {
-		setStatus(worktime_timeout);
-	}
+    void stopWork() {
+        setStatus(worktime_timeout);
+    }
 
-	void startRest() {
-		setStatus(resttime);
-		menu->removeAction(workAction);
-		menu->removeAction(exitAction);
-		menu->addAction(restAction);
-		menu->addAction(exitAction);
-	}
+    void startRest() {
+        setStatus(resttime);
+        menu->removeAction(workAction);
+        menu->removeAction(exitAction);
+        menu->addAction(restAction);
+        menu->addAction(exitAction);
+    }
 
-	void stopRest() {
-		setStatus(rest_timeout);
-	}
+    void stopRest() {
+        setStatus(rest_timeout);
+    }
 
-	void refreshIcon() {
-		QPixmap pixmap = trayIcon->icon().pixmap(16, 16);
-		QPainter painter(&pixmap);
-		QColor color(qrand() % 255, qrand() % 255, qrand() % 255);
-		painter.fillRect(0, 0, 16, 16, color);
-		trayIcon->setIcon(pixmap);
-	}
+    void refreshIcon() {
+        QPixmap pixmap = trayIcon->icon().pixmap(16, 16);
+        QPainter painter(&pixmap);
+        QColor color(qrand() % 255, qrand() % 255, qrand() % 255);
+        painter.fillRect(0, 0, 16, 16, color);
+        trayIcon->setIcon(pixmap);
+    }
 
 public:
-	Controller() {
-		trayIcon = new QSystemTrayIcon();
-		trayIcon->setIcon(QPixmap(16, 16));
-		trayIcon->show();
+    Controller() {
+        trayIcon = new QSystemTrayIcon();
+        trayIcon->setIcon(QPixmap(16, 16));
+        trayIcon->show();
 
-		// 定时器
-		QTimer* timer = new QTimer();
-		QObject::connect(timer, &QTimer::timeout, [&]() {
-			refreshIcon();
-		});
-		timer->start(1000);
+        // 定时器
+        QTimer* timer = new QTimer();
+        QObject::connect(timer, &QTimer::timeout, [&]() {
+            refreshIcon();
+        });
+        timer->start(1000);
 
-		// 右键菜单
-		menu = new QMenu();
-		restAction = new QAction("Rest", menu);
-		menu->addAction(restAction);
-		trayIcon->setContextMenu(menu);
-		QObject::connect(restAction, &QAction::triggered, [&]() {
-			startRest();
-	    });
+        // 右键菜单
+        menu = new QMenu();
+        restAction = new QAction("Rest", menu);
+        menu->addAction(restAction);
+        trayIcon->setContextMenu(menu);
+        QObject::connect(restAction, &QAction::triggered, [&]() {
+            startRest();
+        });
 
-		workAction = new QAction("Work", menu);
-		QObject::connect(workAction, &QAction::triggered, [&]() {
-			startWork();
-		});
+        workAction = new QAction("Work", menu);
+        QObject::connect(workAction, &QAction::triggered, [&]() {
+            startWork();
+        });
 
-		exitAction = new QAction("Exit", menu);
-		menu->addAction(exitAction);
-		trayIcon->setContextMenu(menu);
-		QObject::connect(exitAction, &QAction::trigger, [&]() {
-			a->exit();
-		});
-	}
+        exitAction = new QAction("Exit", menu);
+        menu->addAction(exitAction);
+        trayIcon->setContextMenu(menu);
+        QObject::connect(exitAction, &QAction::trigger, [&]() {
+            a->exit();
+        });
+    }
 
-	void setQApplication(QApplication* a) {
-		this->a = a;
-	}
+    void setQApplication(QApplication* a) {
+        this->a = a;
+    }
 };
 // QMessageBox::information(nullptr, "Information", "menu is clicked!");
